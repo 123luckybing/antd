73777b822e8e2f4c93b22157581b9f7a58da70a2
“zhoufangbing” <1978467950@qq.com>
2018-08-03 11:55:28 +0800

    antd

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router , Route} from 'react-router-dom';
 import './App.css';
+import Home from './Component/Home';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        1
+        <Router>
+          <div>
+            <Route path='/' component={Home}/>
+          </div>
+        </Router>
       </div>
     );
   }
